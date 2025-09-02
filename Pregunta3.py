32e9af3a0e09b87a00c8a3b51bc2d7d9f0642702
Matias Lopez <mlopezme@utem.cl>
2025-09-02 17:21:50 -0400

    Pregunta3.py

---
+++ Pregunta3.py
@@ -0,0 +1,11 @@
+import statistics
+
+from datosEstudiantes import estudiantes
+
+notas_curso = []
+for estudiante in estudiantes:
+    for nota in estudiante["notas"]:
+        notas_curso.append(nota)
+
+moda = statistics.mode(notas_curso)
+print("La nota mas frecuente (moda) considerando todos los estudiantes es", moda)
